86d948d58ed6fb12d18136b84edec6a9a003b4e0
Justin Wang <justinwang46@gmail.com>
2025-01-09 16:15:23 +0000

    working fschat multi-model server

--- fschat.sh
+++ fschat.sh
@@ -1,11 +1,17 @@
 python -m fastchat.serve.controller
 
-python -m fastchat.serve.vllm_worker \
+CUDA_VISIBLE_DEVICES=0 python -m fastchat.serve.vllm_worker \
     --model-path GraySwanAI/Llama-3-8B-Instruct-RR \
-    --device cuda:0
+    --device cuda \
+    --controller-address http://localhost:21001 \
+    --worker-address http://localhost:21002 \
+    --port 21002
 
-python -m fastchat.serve.vllm_worker \
+CUDA_VISIBLE_DEVICES=1 python -m fastchat.serve.vllm_worker \
     --model-path cais/HarmBench-Llama-2-13b-cls \
-    --device cuda:1
+    --device cuda \
+    --controller-address http://localhost:21001 \
+    --worker-address http://localhost:21003 \
+    --port 21003
 
 python -m fastchat.serve.openai_api_server --host localhost --port 8000
--- setup.sh
+++ setup.sh
@@ -2,4 +2,9 @@ ssh-keygen -t ed25519
 mamba create -n bon python=3.11.7
 sudo apt install ffmpeg
 pip install -r requirements.txt
-pip install vllm fschat
+pip install vllm==0.6.2
+cd ..
+git clone https://github.com/lm-sys/FastChat.git
+cd FastChat
+pip install -e ".[model_worker]"
+cd ../bon-jailbreaking
